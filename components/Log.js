ca914da9dd05782b3470b8a67e30a4a9b0a6ea41
scottzyang <scottzyang@gmail.com>
2024-04-29 16:54:42 -0500

    Render data info on Log component

--- components/Log.js
+++ components/Log.js
@@ -1,12 +1,20 @@
 import { View, Text } from "react-native";
 
-export default function Log() {
+export default function Log({ route }) {
+  const { data } = route.params;
+
   const showValue = () => {
     AsyncStorage.getItem("cat").then((value) => setGetValue(value));
   };
   return (
     <View>
       <Text> Log Details Page</Text>
+      <Text>Forage Name: {data.value.forageName}</Text>
+      <Text>Description: {data.value.journalEntry}</Text>
+      <Text>Total Found: {data.value.total}</Text>
+      <Text>Weather: {data.value.weatherConditions}</Text>
+      <Text>Date: {data.value.date}</Text>
+      <Text>Time: {data.value.time}</Text>
     </View>
   );
 }
