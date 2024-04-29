1caeca186ce07fdcd20ddc76c46341c2141b768a
scottzyang <scottzyang@gmail.com>
2024-04-28 20:14:57 -0500

    Set up AsyncStorage

--- components/LogForm.js
+++ components/LogForm.js
@@ -1,9 +1,46 @@
 import { View } from "react-native";
+import { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function LogForm() {
+  const [text, setText] = useState("");
+  const [getValue, setGetValue] = useState("");
+
+  const saveValue = () => {
+    if (text) {
+      AsyncStorage.setItem("cat", text);
+      setText("");
+      alert("Data saved BRUH");
+    } else {
+      alert("Please input data. BRUH");
+    }
+  };
+  const showValue = () => {
+    AsyncStorage.getItem("cat").then((value) => setGetValue(value));
+  };
+
   return (
-    <View>
-      <Text>This is the form to enter logs!</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Log Form for Mushrooms</Text>
+      <TextInput
+        placeholder="What did you find?"
+        value={text}
+        onChangeText={(e) => setText(e)}
+      />
+      <TouchableOpacity onPress={saveValue}>
+        <Text>SAVE BRUH</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={showValue}>
+        <Text>SEE BRUH</Text>
+      </TouchableOpacity>
+      <Text>{getValue}</Text>
+    </SafeAreaView>
   );
 }
