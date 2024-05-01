3d3d2af22bbe47e656c273295aca1f3adb50b77d
scottzyang <scottzyang@gmail.com>
2024-05-01 16:27:57 -0500

    Initial slice set up

--- features/fungiSlice.js
+++ features/fungiSlice.js
@@ -1,9 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  forageName: "",
+  journalEntry: "",
+  total: "",
+  weatherConditions: "",
+  parsedData: [],
+  savingData: false,
+};
+
 export const fungiSlice = createSlice({
   name: "fungi",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    toggleSavingData: (state) => {
+      state.savingData = !state.savingData;
+    },
+    updateParsedData: (state, action) => {
+      state.parsedData = action.payload;
+    },
+    updateForageName: (state, action) => {
+      state.forageName = action.payload;
+    },
+    updateJournalEntry: (state, action) => {
+      state.journalEntry = action.payload;
+    },
+    updateTotal: (state, action) => {
+      state.total = action.payload.total;
+    },
+    updateWeatherConditions: (state, action) => {
+      state.weatherConditions = action.payload;
+    },
+  },
 });
 
+// export reducer
 export default fungiSlice.reducer;
+
+// export actions
+export const {
+  toggleSavingData,
+  updateForageName,
+  updateJournalEntry,
+  updateParsedData,
+  updateTotal,
+  updateWeatherConditions,
+} = fungiSlice.actions;
+
+// export selectors
+export const selectForageName = (state) => state.fungi.forageName;
+export const selectJournalEntry = (state) => state.fungi.journalEntry;
+export const selectTotal = (state) => state.fungi.total;
+export const selectWeatherConditions = (state) => state.fungi.weatherConditions;
+export const selectParsedData = (state) => state.fungi.parsedData;
+export const selectSavingData = (state) => state.fungi.savingData;
